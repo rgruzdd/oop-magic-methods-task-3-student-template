dd18b07861c3f3888b9fb34d928b55fb66ac20a8
Gruzin <gruzd.roman@inbox.ru>
2023-04-26 15:01:32 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -3,32 +3,198 @@ from typing import Type
 
 
 class Currency:
-    """
-    1 EUR = 2 USD = 100 GBP
-
-    1 EUR = 2 USD    ;  1 EUR = 100 GBP
-    1 USD = 0.5 EUR  ;  1 USD = 50 GBP
-    1 GBP = 0.02 USD ;  1 GBP = 0.01 EUR
-    """
 
     def __init__(self, value: float):
-        pass
+        self.value = value
 
     @classmethod
     def course(cls, other_cls: Type[Currency]) -> str:
-        raise NotImplementedError
+        return ''
 
     def to_currency(self, other_cls: Type[Currency]):
-        raise NotImplementedError
-
+        return ''
 
 class Euro(Currency):
-    pass
+    def __init__(self, value: float):
 
+        super().__init__(value)
+
+    @classmethod
+    def course(cls, other_cls: Type[Currency]) -> str:
+        current_currency = str(1) + ' EUR'
+        foreign_currency = 0
+        if other_cls == Dollar:
+            foreign_currency = str(2.0) + ' USD'
+        elif other_cls == Pound:
+            foreign_currency = str(100.0) + ' GBP'
+        return foreign_currency + ' for ' + current_currency
+
+    def to_currency(self, other_cls: Type[Currency]):
+        current_currency = int(self.value)
+        foreign_currency = 0
+        if other_cls == Euro:
+            foreign_currency = int(current_currency)
+            string = str(float(foreign_currency)) + ' EUR'
+            return string
+        elif other_cls == Dollar:
+            foreign_currency = (int(current_currency) * 2)
+            string = str(float(foreign_currency)) + ' USD'
+            return string
+        elif other_cls == Pound:
+            foreign_currency = (int(current_currency) * 100)
+        string = str(float(foreign_currency)) + ' GBR'
+        return string
+
+    def __str__(self):
+        return str(self.value) + ' EUR'
+
+    def __add__(self, other):
+        if type(other) == Euro:
+            new_value = float(self.value + other.value)
+        elif type(other) != Euro:
+            new_value = float(self.value + float(other.to_currency(Euro).split(' ')[0]))
+        return str(new_value) + ' EUR'
+
+    def __eq__(self, other):
+        if type(other) == Euro:
+            new_value = self.value == other.value
+        elif type(other) != Euro:
+            new_value = self.value == float(other.to_currency(Euro).split(' ')[0])
+        return new_value
+
+    def __lt__(self, other):
+        if type(other) == Euro:
+            new_value = self.value < other.value
+        elif type(other) != Euro:
+            new_value = self.value < float(other.to_currency(Euro).split(' ')[0])
+        return new_value
+
+    def __gt__(self, other):
+        if type(other) == Euro:
+            new_value = self.value > other.value
+        elif type(other) != Euro:
+            new_value = self.value > float(other.to_currency(Euro).split(' ')[0])
+        return new_value
 
 class Dollar(Currency):
-    pass
+    def __init__(self, value: float):
+        super().__init__(value)
 
+    @classmethod
+    def course(cls, other_cls: Type[Currency]) -> str:
+        current_currency = str(1) + ' USD'
+        foreign_currency = 0
+        if other_cls == Euro:
+            foreign_currency = str(0.5) + ' EUR'
+        elif other_cls == Pound:
+            foreign_currency = str(50.0) + ' GBP'
+        return foreign_currency + ' for ' + current_currency
+
+    def to_currency(self, other_cls: Type[Currency]):
+        current_currency = int(self.value)
+        foreign_currency = 0
+        if other_cls == Euro:
+            foreign_currency = (int(current_currency) * 0.5)
+            string = str(float(foreign_currency)) + ' EUR'
+            return string
+        elif other_cls == Dollar:
+            foreign_currency = int(current_currency)
+            string = str(float(foreign_currency)) + ' USD'
+            return string
+        elif other_cls == Pound:
+            foreign_currency = (int(current_currency) * 50)
+        string = str(float(foreign_currency)) + ' GBR'
+        return string
+
+    def __str__(self):
+        return str(self.value) + ' USD'
+
+    def __add__(self, other):
+        if type(other) == Dollar:
+            new_value = float(self.value + other.value)
+        elif type(other) != Dollar:
+            new_value = float(self.value + float(other.to_currency(Dollar).split(' ')[0]))
+        return str(new_value) + ' USD'
+
+    def __eq__(self, other):
+        if type(other) == Dollar:
+            new_value = self.value == other.value
+        elif type(other) != Dollar:
+            new_value = self.value == float(other.to_currency(Dollar).split(' ')[0])
+        return new_value
+
+    def __lt__(self, other):
+        if type(other) == Dollar:
+            new_value = self.value < other.value
+        elif type(other) != Dollar:
+            new_value = self.value < float(other.to_currency(Dollar).split(' ')[0])
+        return new_value
+
+    def __gt__(self, other):
+        if type(other) == Dollar:
+            new_value = self.value > other.value
+        elif type(other) != Dollar:
+            new_value = self.value > float(other.to_currency(Dollar).split(' ')[0])
+        return new_value
 
 class Pound(Currency):
-    pass
+    def __init__(self, value: float):
+        super().__init__(value)
+
+    @classmethod
+    def course(cls, other_cls: Type[Currency]) -> str:
+        current_currency = str(1) + ' GBP'
+        foreign_currency = 0
+
+        if other_cls == Dollar:
+            foreign_currency = str(0.02) + ' USD'
+        elif other_cls == Euro:
+            foreign_currency = str(0.01) + ' EUR'
+        return foreign_currency + ' for ' + current_currency
+
+    def to_currency(self, other_cls: Type[Currency]):
+        current_currency = int(self.value)
+        foreign_currency = 0
+        if other_cls == Euro:
+            foreign_currency = (int(current_currency) * 0.01)
+            string = str(float(foreign_currency)) + ' EUR'
+            return string
+        elif other_cls == Dollar:
+            foreign_currency = (int(current_currency) * 0.02)
+            string = str(float(foreign_currency)) + ' USD'
+            return string
+        elif other_cls == Pound:
+            foreign_currency = (int(current_currency) * 1)
+        string = str(float(foreign_currency)) + ' GBR'
+        return string
+
+    def __str__(self):
+        return str(self.value) + ' GBP'
+
+    def __add__(self, other):
+        if type(other) == Pound:
+            new_value = float(self.value + other.value)
+        elif type(other) != Pound:
+            new_value = float(self.value + float(other.to_currency(Pound).split(' ')[0]))
+        return str(new_value) + ' GBR'
+
+    def __eq__(self, other):
+        if type(other) == Pound:
+            new_value = self.value == other.value
+        elif type(other) != Pound:
+            new_value = self.value == float(other.to_currency(Pound).split(' ')[0])
+        return new_value
+
+    def __lt__(self, other):
+        if type(other) == Pound:
+            new_value = self.value < other.value
+        elif type(other) != Pound:
+            new_value = self.value < float(other.to_currency(Pound).split(' ')[0])
+        return new_value
+
+    def __gt__(self, other):
+        if type(other) == Pound:
+            new_value = self.value > other.value
+        elif type(other) != Pound:
+            new_value = self.value > float(other.to_currency(Pound).split(' ')[0])
+        return new_value
